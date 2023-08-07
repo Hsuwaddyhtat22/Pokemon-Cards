928ec96d473295dea6fddf339fe936ffd1f87b92
Hsu Waddy <hsuwady2001@gmail.com>
2023-08-07 18:13:59 +0630

    update-style

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Leader',
-            template: './src/index.html',
-        }),
-    ],
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        }, {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        }],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Leader',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }, {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    }],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
