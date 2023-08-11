4537a0b1afd41ecd443ec4b677564b5a76b75337
dvcorreia1984 <victor@onsleer.co.za>
2023-08-11 15:21:45 +0200

    Fixed EsLint Errors

--- src/modules/cards.js
+++ src/modules/cards.js
@@ -1,22 +1,32 @@
-/* eslint-disable no-await-in-loop */
 // cards.js
 
 const fetchOriginalPokemonData = async () => {
   const numberOfPokemon = 9; // Change this to the desired number of Pokémon
   const originalPokemonCards = [];
 
+  const fetchPromises = [];
   for (let i = 1; i <= numberOfPokemon; i += 1) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-    const data = await response.json();
+    fetchPromises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`));
+  }
+  const responses = await Promise.all(fetchPromises);
+  const jsonResponses = await Promise.all(responses.map((response) => response.json()));
 
-    const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}`);
-    const speciesData = await speciesResponse.json();
+  const speciesFetchPromises = [];
+  for (let i = 1; i <= numberOfPokemon; i += 1) {
+    speciesFetchPromises.push(fetch(`https://pokeapi.co/api/v2/pokemon-species/${i}`));
+  }
+  const speciesResponses = await Promise.all(speciesFetchPromises);
 
+  const speciesJsonPromises = speciesResponses.map((response) => response.json());
+  const speciesJsonResponses = await Promise.all(speciesJsonPromises);
+
+  for (let i = 0; i < numberOfPokemon; i += 1) {
+    const speciesData = speciesJsonResponses[i];
     const description = speciesData.flavor_text_entries.find((entry) => entry.language.name === 'en').flavor_text;
 
     originalPokemonCards.push({
-      name: data.name,
-      id: data.id,
+      name: jsonResponses[i].name,
+      id: jsonResponses[i].id,
       description,
     });
   }
--- src/modules/commentCounter.test.js
+++ src/modules/commentCounter.test.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { JSDOM } from 'jsdom';
 import countComments from './mentCounter.js';
 
