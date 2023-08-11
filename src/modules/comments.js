f7a3ba15d111d56771dfdafe9a05adac50879d9a
Hsu Waddy <hsuwady2001@gmail.com>
2023-08-11 19:34:14 +0630

    Add-arrow-es6-sentence

---
+++ src/modules/comments.js
@@ -0,0 +1,38 @@
+// comments.js
+import { baseURL, appId } from './involveapi.js';
+
+const getComments = async(pokemonName) => {
+    try {
+        const response = await fetch(`${baseURL}apps/${appId}/comments/?item_id=${pokemonName}`);
+        if (response.status === 200) {
+            const apiCommentsData = await response.json();
+            return apiCommentsData;
+        }
+    } catch (error) {
+        return null;
+    }
+};
+
+const addComment = async(pokemonName, username, comment) => {
+    try {
+        const response = await fetch(`${baseURL}apps/${appId}/comments/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                item_id: pokemonName,
+                username,
+                comment,
+            }),
+        });
+        if (response.status === 201) {
+            return true;
+        }
+        return false;
+    } catch (error) {
+        return false;
+    }
+};
+
+export { getComments, addComment };
