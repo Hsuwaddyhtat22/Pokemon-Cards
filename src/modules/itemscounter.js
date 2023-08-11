a6f8394c9ef4f6417fa4f2cb838780cda792888b
Victor Correia <victor@onsleer.co.za>
2023-08-11 12:12:17 +0200

    Revert "Pokemon/set"

--- src/modules/itemscounter.js
+++
@@ -1,6 +0,0 @@
-const countItems = () => {
-  const itemCounter = document.getElementById('itemCounter');
-  const items = document.querySelectorAll('.item');
-  itemCounter.textContent = `Total Cards: ${items.length}`;
-};
-export default countItems;
